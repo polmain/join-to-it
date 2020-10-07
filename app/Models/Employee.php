3e1b138658c10d9f70f46a7779337d8ce6f83420
Polmain <polmain8@gmail.com>
2020-10-07 12:07:48 +0300

    Add relationships to Company and Employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
+
+    public function company(){
+    	return $this->belongsTo('App\Models\Company');
+	}
 }
